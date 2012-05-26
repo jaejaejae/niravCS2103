c6c1985f898cd051e840afe1403ca799da67fb12
nirav gandhi <ngandhi1993@gmail.com>
2012-05-27 00:32:57 +0800

    Updated. Every function in storage manager is not of boolean type except exportToTxt.

---
+++ NewStorage/src/StorageTests/TaskHashMapTest.java
@@ -0,0 +1,49 @@
+package StorageTests;
+
+import data.DateTime;
+import data.Task;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import data.TaskHashMap;
+public class TaskHashMapTest {
+	
+	DateTime start = new DateTime(2012,5,30,18,0,0);
+	DateTime end = new DateTime(2012,5,30,18,30,0);
+	Task first=new Task("Eat dinner","at mcd",start,end,"weekly");
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGenerateUniqueId() {
+		TaskHashMap test=new TaskHashMap();
+		test.addTask(first);
+		assertEquals("",test.generateUniqueId(first));
+	}
+
+	@Test
+	public final void testDeleteTaskById() {
+	TaskHashMap test=new TaskHashMap();
+	test.addTask(first);
+	test.deleteTaskById(first.getTaskId());
+	assertEquals(0,test.getKeySet().size());
+	}
+
+	@Test
+	public final void testClearHashMap() {
+		TaskHashMap test=new TaskHashMap();
+		test.addTask(first);
+		test.addTask(first);
+		assertEquals(2,test.getKeySet().size());
+		test.clearHashMap();
+		assertEquals(0,test.getKeySet().size());
+	}
+
+}
